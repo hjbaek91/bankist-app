40cfa9fd84513f81f62e5daf55a0e181257beaa7
HyunJun Baek <69138182+hjbaek91@users.noreply.github.com>
2023-05-10 15:33:22 +0900

    Implemented transfer functionality

--- script.js
+++ script.js
@@ -153,4 +153,12 @@ btnLogin.addEventListener("click", function (e) {
   }
 });
 
+btnTransfer.addEventListener("click", function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    (acc) => acc.username === inputTransferTo.value
+  );
+});
+
 /////////////////////////////////////////////////
