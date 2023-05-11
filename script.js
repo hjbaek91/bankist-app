18457f5997acd51531776760fe885d22d1ea2c36
HyunJun Baek <69138182+hjbaek91@users.noreply.github.com>
2023-05-11 10:18:16 +0900

    Implemented loan request functionality

--- script.js
+++ script.js
@@ -181,6 +181,25 @@ btnTransfer.addEventListener("click", function (e) {
   }
 });
 
+// Loan functionality
+btnLoan.addEventListener("click", function (e) {
+  e, preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = "";
+});
+
 // Closing an account
 btnClose.addEventListener("click", function (e) {
   e.preventDefault();
