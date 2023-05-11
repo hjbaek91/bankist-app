fe07b440b1b0f4e733929dfcd123b1982537eeac
HyunJun Baek <69138182+hjbaek91@users.noreply.github.com>
2023-05-11 09:11:24 +0900

    Implemented account closing functionality

--- script.js
+++ script.js
@@ -157,6 +157,7 @@ btnLogin.addEventListener("click", function (e) {
   }
 });
 
+// Implementing transfer functionality
 btnTransfer.addEventListener("click", function (e) {
   e.preventDefault();
   const amount = Number(inputTransferAmount.value);
@@ -178,6 +179,28 @@ btnTransfer.addEventListener("click", function (e) {
     // Update UI
     updateUI(currentAccount);
   }
+});
+
+// Closing an account
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = "";
 });
 
 /////////////////////////////////////////////////
