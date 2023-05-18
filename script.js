2f105974fee0a870521b743c5ef9c58e10a4d5cc
HyunJun Baek <69138182+hjbaek91@users.noreply.github.com>
2023-05-18 11:22:55 +0900

    Displaying date status based on time passed

--- script.js
+++ script.js
@@ -87,10 +87,15 @@ const formatMovementDate = function (date) {
 
   const daysPassed = calcDaysPassed(new Date(), date);
 
-  const day = `${date.getDate()}`.padStart(2, 0);
-  const month = `${date.getMonth() + 1}`.padStart(2, 0);
-  const year = date.getFullYear();
-  return `${month}/${day}/${year}`;
+  if (daysPassed === 0) return "Today";
+  if (daysPassed === 1) return "Yesterday";
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+    return `${month}/${day}/${year}`;
+  }
 };
 
 const displayMovements = function (acc, sort = false) {
